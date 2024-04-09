52856d42b977ebd24b8d01ae97ee62fc54c4570a
Juan Palma <juan.a.palma.vidal@gmail.com>
2024-04-08 20:19:42 -0600

    Plantilla inicial

---
+++ assets/js/owner/main.js
@@ -0,0 +1,17 @@
+
+function iniciar() {
+	
+
+}
+
+
+
+
+// iniciar la solicitud de los modulos y la ejecucion inicial del sistema.
+//importamos los archivos y librerias necesarios
+requirejs.config({
+	baseUrl: "assets/js/owner",
+	paths: { a: "../animaciones", l: "../librerias", n: "/node_modules"},
+});
+requirejs(["l/modernizr", "l/precarga", "validaciones", "alertas", "peticiones"], iniciar);
+
