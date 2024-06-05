0e03e366e4a01f1ce7e0b31b130994f1e518182d
Juan Palma <juan.a.palma.vidal@gmail.com>
2024-06-05 01:43:01 -0600

    minify code

--- assets/js/owner/main-edit.js
+++ assets/js/owner/main-edit.js
@@ -7,156 +7,24 @@ idagl.elementos = {};
 const el = idagl.elementos;
 
 
+// Funcion de niver superiro debouce.
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+}
+
 
 async function iniciar() {
-	//Animaciones GSAP
-	gsap.registerPlugin(ScrollTrigger);
-	gsap.registerPlugin(ScrollToPlugin);
-	gsap.registerPlugin(Flip);
-
-	//Cortar java para pruebas o ajustes
-	// const loadingBox = document.getElementById("loadingBox");
-	// if(loadingBox){
-	// 	gsap.to(loadingBox, {
-	// 		duration: 0.3,
-	// 		opacity: 0,
-	// 		display: "none"
-	// 	});
-	// 	document.body.style.overflow = "auto";
-	// }
-	// return;
-
-	// codigo para actualizar animaciones GSAP.
-	function debounce(func, wait, immediate) {
-		var timeout;
-		return function() {
-			var context = this, args = arguments;
-			var later = function() {
-				timeout = null;
-				if (!immediate) func.apply(context, args);
-			};
-			var callNow = immediate && !timeout;
-			clearTimeout(timeout);
-			timeout = setTimeout(later, wait);
-			if (callNow) func.apply(context, args);
-		};
-	}
-
-
-
-
-
-
-	//Funciones para habilitar el menu mobile
-	function btnMobileActive(e) {
-		this.classList.toggle('activo');
-	}
-	const btnMobile = document.getElementById('menuBoxMobile');
-	if(btnMobile){
-		btnMobile.addEventListener('click', btnMobileActive);
-
-		//Animacion y control de submenus
-		const botonMenu = document.getElementById("servicioBtn");
-		let submenus = gsap.utils.toArray(".submenu1Box a");
-		if(submenus){
-			const sumN = submenus.length;
-			let submenuAbierto = false;
-
-			submenus.forEach(s => {
-				const medidas = s.getBoundingClientRect();
-				s.medidas = medidas;
-				s.style.display = 'none';
-			});
-
-			const timeline = gsap.timeline({ paused: true });
-			botonMenu.addEventListener("click", () => {
-				if (submenuAbierto) {
-					timeline.reverse();
-				} else {
-					submenus.forEach((elemento) => {
-					gsap.set(elemento, { rotation: 0 }); // Restablece la rotación antes de la animación
-					});
-					
-					timeline.clear(); // Limpia la línea de tiempo existente
-					
-					timeline.to(submenus, {
-						y: (index, s) => ((sumN - 1 - index) * (s.medidas.height + 8) + 24),
-						duration:0.45,
-						rotation: () => gsap.utils.random(-4, 4),
-						autoAlpha: 1,
-						display:"block",
-						ease: "power4.ease.inOut",
-						stagger:0.20
-					});
-					
-					timeline.play();
-				}
-				submenuAbierto = !submenuAbierto;
-			});
-	}
-
-
-	
-
-
-		// timeline.to(submenus, {
-		// 	y: (index, s) => ((sumN - 1 - index) * (s.medidas.height + 8) + 24),
-		// 	duration:0.45,
-		// 	rotation: () => gsap.utils.random(-4, 4),
-		// 	autoAlpha: 1,
-		// 	display:"block",
-		// 	ease: "power4.ease.inOut",
-		// 	stagger:0.20
-		// });
-
-		// document.getElementById("servicioBtn").addEventListener("click", () => {
-		// 	if (submenuAbierto) {
-		// 		timeline.reverse();
-		// 	} else {
-		// 		submenus.forEach((elemento) => {
-		// 			gsap.set(elemento, { rotation: 0 }); // Restablece la rotación antes de la animación
-		// 		});
-		// 		timeline.play();
-		// 	}
-		// 	submenuAbierto = !submenuAbierto;
-		// });
-	}
-	
-
-
-
-
-
-
-	// Animaicion para los enlaces scrollin to del sitio
-	const menuLinksScrollingTo = gsap.utils.toArray(".scrollingTo");
-	if(menuLinksScrollingTo){
-		menuLinksScrollingTo.forEach(link => {
-			link.addEventListener('click', e => {
-				e.preventDefault();
-				
-				let target;
-				if (link.tagName === 'BUTTON') {
-					target = document.querySelector(link.dataset.href);
-				} else if (link.tagName === 'A') {
-					target = document.querySelector(link.getAttribute('href'));
-				}				
-	
-				if(target) {
-					gsap.to(window, {
-						duration: 2,
-						scrollTo: { y: target, offsetY: 100 },
-						ease: 'power3.inOut'
-					});
-				}
-			});
-		});
-	}
-
-	
-
-
-
 	//activar estilos o efectos para mobile css
 	//habilitar funciones para moviles:
 	if ((el.mobile = /Mobile/i.test(navigator.userAgent))) {
@@ -170,57 +38,10 @@ async function iniciar() {
 		}
 	}
 
+
+
 	
-	function pantallasAniFunc(){
-		if(!document.getElementById("pantallaBox")) return;
-		
-		let pantallasTL = gsap.timeline({});
-		pantallasTL.to("#pantalla1", {
-			y: () => `+=${document.getElementById("pantalla1").offsetHeight*-1}`,
-			ease:"none",
-			duration: 2
-		});
-		
-		pantallasTL.to("#pantalla2", {
-			// backgroundColor: "#100035",
-			ease:"none",
-			duration:4
-		}, 1.5);
-
-		let pantalla3TL = gsap.timeline({});
-			pantalla3TL.to("#pantalla3", {
-				x: () => `+=${document.getElementById("pantalla3").offsetWidth*-1}`,
-				ease:"none",
-				duration: 2
-			});
-			pantalla3TL.to("#pantalla3 .frase", {
-				x: () => `+=${document.getElementById("pantalla3").offsetWidth}`,
-				ease:"none",
-				duration: 2
-			}, "<");
-
-		pantallasTL.add(pantalla3TL, "-=3");
-
-		pantallasTL.to({}, {
-			duration: 1
-		});
-
-
-		let pantallaBox = document.getElementById("pantallaBox");
-		ScrollTrigger.create({
-			animation: pantallasTL,
-			trigger: pantallaBox,
-			pin:true,
-			anticipatePin:1,
-			scrub:2,
-			start:"top top",
-			end: () => document.getElementById("pantallaBox").offsetHeight,
-			invalidateOnRefresh: true,
-			fastScrollEnd: true
-			// markers:true
-		});
-	}
-	pantallasAniFunc();
+	
 
 	function updateAnimations() {
 		// // Asegúrate de que las animaciones y ScrollTriggers existan antes de intentar matarlas
@@ -245,7 +66,7 @@ async function iniciar() {
 	}
 
 	//window.addEventListener('resize', debounce(updateAnimations, 250));
-	window.addEventListener('orientationchange', debounce(updateAnimations, 250));
+	//window.addEventListener('orientationchange', debounce(updateAnimations, 250));
 	//window.addEventListener('resize', debounce(updateAnimations, 250));
 	// window.addEventListener('resize', debounce(updatePantallaStyle, 250));
 
@@ -480,15 +301,6 @@ async function iniciar() {
 
 	});
 
-	if(document.querySelector("#pantalla1 .intro")){
-		gsap.to("#pantalla1 .intro", {
-			duration:0.65,
-			ease: "power3.ease.inOut",
-			y: 0,
-			autoAlpha: 1
-		});
-	}
-
 
 
 
@@ -534,8 +346,140 @@ async function iniciar() {
 	});
 	
 
+}
 
-	function activeFlat(e){
+
+
+
+
+
+
+
+
+
+//Colocacion de correo con cierto nivel de seguridad
+function openMailer(event) {
+	event.preventDefault();
+
+	var url = encodeURIComponent(destinatario = atob("aG9sYUB2ZW50b21hcXVpYS5jb20="));
+	window.location.href = "mailto:" + url;
+}
+
+
+
+
+
+
+let dbDudas = [];
+dbDudas.push({pregunta:"¿Necesito tener redes sociales para me ayuden a aumentar mis ventas?", respuesta:"Sí, ya que forman parte de nuestro sistema comprobado para que vendas más."});
+dbDudas.push({pregunta:"Qué redes sociales “ocupo”?", respuesta:"Por lo menos Facebook. Dependiendo del giro de tu negocio, también puedes necesitar Instagram. Tenemos comprobado que la combinación de ambas es una receta segura para lograr ventas."});
+dbDudas.push({pregunta:"¿Uds. pueden ayudarme a crear mis redes sociales en caso de no tenerlas?", respuesta:"Por supuesto. Te llevaremos paso a paso en el proceso y te daremos algunos consejos para que de aquí en adelante puedas crear todas las que desees."});
+dbDudas.push({pregunta:"¿Las quesadillas deben llevar queso?", respuesta:"Nuestro abogados recomiendan no entrar en esas polémicas, por lo que nuestra respuesta es “sigue tu corazón”."});
+dbDudas.push({pregunta:"Tengo redes, pero no tengo página web. ¿Es  necesaria?", respuesta:"No. Podemos incrementar tus ventas sólo con tus redes sociales. Pero considera que contar con un sitio web le da mucha más credibilidad a cualquier negocio."});
+dbDudas.push({pregunta:"Me es difícil crear los textos y contenido para mi página de internet. ¿Uds. me ayudan con eso?", respuesta:"Claro!! Te ayudamos con imágenes, íconos y los textos más seductores para tu público."});
+dbDudas.push({pregunta:"¿El único afortunado de contar con sus servicios es Jalisco?", respuesta:"Gracias a la tecnología podemos brindar servicios a cualquier parte de la República. Si gustas que vayamos a tomar algo, nuestro equipo se encuentra en CDMX y Guadalajara ;D"});
+dbDudas.push({pregunta:"¿Vienen, voy, nos vemos debajo del reloj, o cómo está la cosa?", respuesta:"Nosotros preferimos visitarte para conocer tu negocio y verlo en acción. Mientras más nos empapemos de lo que haces, tendremos más herramientas para desarrollar una mejor estrategia. "});
+
+
+
+
+
+
+
+
+
+
+
+
+async function precargaRun(){
+	return new Promise((r, e) => {
+		setTimeout(() => {
+			const precarga = new Precarga();
+			precarga.userFunc = iniciar;
+			precarga.run();
+			r('la precarga de imagenes esta en curso...');
+		}, retrasoPregargaImg);
+	});
+};
+//Secciono de funciones:
+
+async function toolsPimeraCarga(){
+	//Funciones para habilitar el menu mobile
+	function btnMobileActive(e) {
+		this.classList.toggle('activo');
+	}
+	const btnMobile = document.getElementById('menuBoxMobile');
+	if(btnMobile){
+		btnMobile.addEventListener('click', btnMobileActive);
+
+		//Animacion y control de submenus
+		const botonMenu = document.getElementById("servicioBtn");
+		let submenus = gsap.utils.toArray(".submenu1Box a");
+		if(submenus){
+			const sumN = submenus.length;
+			let submenuAbierto = false;
+
+			submenus.forEach(s => {
+				const medidas = s.getBoundingClientRect();
+				s.medidas = medidas;
+				s.style.display = 'none';
+			});
+
+			const timeline = gsap.timeline({ paused: true });
+			botonMenu.addEventListener("click", () => {
+				if (submenuAbierto) {
+					timeline.reverse();
+				} else {
+					submenus.forEach((elemento) => {
+					gsap.set(elemento, { rotation: 0 }); // Restablece la rotación antes de la animación
+					});
+					
+					timeline.clear(); // Limpia la línea de tiempo existente
+					
+					timeline.to(submenus, {
+						y: (index, s) => ((sumN - 1 - index) * (s.medidas.height + 8) + 24),
+						duration:0.45,
+						rotation: () => gsap.utils.random(-4, 4),
+						autoAlpha: 1,
+						display:"block",
+						ease: "power4.ease.inOut",
+						stagger:0.20
+					});
+					
+					timeline.play();
+				}
+				submenuAbierto = !submenuAbierto;
+			});
+		}
+	}
+
+	// Animaicion para los enlaces scrollin to del sitio
+	const menuLinksScrollingTo = gsap.utils.toArray(".scrollingTo");
+	if(menuLinksScrollingTo){
+		menuLinksScrollingTo.forEach(link => {
+			link.addEventListener('click', e => {
+				e.preventDefault();
+				
+				let target;
+				if (link.tagName === 'BUTTON') {
+					target = document.querySelector(link.dataset.href);
+				} else if (link.tagName === 'A') {
+					target = document.querySelector(link.getAttribute('href'));
+				}				
+	
+				if(target) {
+					gsap.to(window, {
+						duration: 2,
+						scrollTo: { y: target, offsetY: 100 },
+						ease: 'power3.inOut'
+					});
+				}
+			});
+		});
+	}
+
+
+	function circuloIndicadorAni(e){
 		const indicador = document.querySelector("#preguntasBoxG .preguntas .indicador");
 		const padre = e.trigger.parentElement;
 		const state = Flip.getState([indicador, e.trigger]);
@@ -601,99 +545,184 @@ async function iniciar() {
 				end: "bottom 70%",
 				invalidateOnRefresh: true,
 				fastScrollEnd: true,
-				onEnter: activeFlat,
-				onEnterBack: activeFlat
+				onEnter: circuloIndicadorAni,
+				onEnterBack: circuloIndicadorAni
 				// markers:true
 			});
 		});
 	};
-	
+}
 
 
-
-
-
-
-
-	//codigo para mostrar el sitio despues de que esta cargado
-	const loadingBox = document.getElementById("loadingBox");
-	if(loadingBox){
-		gsap.to(loadingBox, {
-			duration: 0.3,
-			opacity: 0,
-			display: "none"
+async function pantalla1Ani(){
+	if(document.querySelector("#pantalla1 .intro")){
+		gsap.to("#pantalla1 .intro", {
+			duration:0.65,
+			ease: "power3.ease.inOut",
+			y: 0,
+			autoAlpha: 1
 		});
-		document.body.style.overflow = "auto";
-		if(globalSeccion == "politicas"){
-			document.querySelector(".contenido").style.opacity = "1";
+		gsap.to("#pantalla1 .graficas .grafica", {
+			duration:0.65,
+			ease: "power3.ease.inOut",
+			x: 0,
+			autoAlpha: 1,
+			stagger:0.15
+		});
+		return "Se dispararon las animaciones de pantalla 1";
+	} else{
+		return "no se encontro #pantalla1 .intro";
+	}
+}
+
+async function pantallasAni(){
+	if(!document.getElementById("pantallaBox")) return "No se encuentra una estructura html valida para las pantallas";
+	
+	let pantallasTL = gsap.timeline({});
+	pantallasTL.to("#pantalla1", {
+		y: () => `+=${document.getElementById("pantalla1").offsetHeight*-1}`,
+		ease:"none",
+		duration: 2
+	});
+	
+	pantallasTL.to("#pantalla2", {
+		// backgroundColor: "#100035",
+		ease:"none",
+		duration:4
+	}, 1.5);
+
+	let pantalla3TL = gsap.timeline({});
+		pantalla3TL.to("#pantalla3", {
+			x: () => `+=${document.getElementById("pantalla3").offsetWidth*-1}`,
+			ease:"none",
+			duration: 2
+		});
+		pantalla3TL.to("#pantalla3 .frase", {
+			x: () => `+=${document.getElementById("pantalla3").offsetWidth}`,
+			ease:"none",
+			duration: 2
+		}, "<");
+
+	pantallasTL.add(pantalla3TL, "-=3");
+
+	pantallasTL.to({}, {
+		duration: 1
+	});
+
+
+	let pantallaBox = document.getElementById("pantallaBox");
+	ScrollTrigger.create({
+		animation: pantallasTL,
+		trigger: pantallaBox,
+		pin:true,
+		anticipatePin:1,
+		scrub:2,
+		start:"top top",
+		end: () => document.getElementById("pantallaBox").offsetHeight,
+		invalidateOnRefresh: true,
+		fastScrollEnd: true
+		// markers:true
+	});
+
+	return "se ejecuto codigo de animacion de pantallas";
+}
+
+
+async function pantallasShow(){
+	return new Promise((r, e) => {
+		const loadingBox = document.getElementById("loadingBox");
+		if(loadingBox){
+			gsap.to(loadingBox, {
+				duration: 0.4,
+				autoAlpha: 0,
+				display: "none"
+			});
+			document.body.style.overflow = "auto";
+			if(globalSeccion == "politicas"){
+				gsap.to(".contenido", {
+					duration: 0.8,
+					autoAlpha: 1
+				});
+			} else{
+				const lista = new Array(document.getElementById("pantallaG"), document.querySelector("nav"));
+				gsap.to(lista, {
+					duration: 0.8,
+					autoAlpha: 1,
+					onComplete: () => { r("Pantallas animadas"); }
+				});
+			}
+			//r('Las pantallas estan activadas');
 		} else{
-			document.getElementById("pantallaG").style.opacity = "1";
-			document.querySelector("nav").style.opacity = "1";
+			e('no se encontro el elemento #loadingBox');
 		}
+	})
+};
+
+
+
+
+
+
+
+
+
+
+
+/*
+Funcion para hacer la carga de los recursos primordiales en la primera etapa de inicio del sitio, cargu aqui lo primero que deba de ver el usuario y las funciones mas primordiales y becesarias del sitio.
+*/
+async function cargaSecundaria(){
+	try {
+		await precargaRun();
+	} catch (error) {
+		console.error('Error ejecutando cargaSecundaria:', error);
+	}
+	return 'cargaSecundaria terminada.';
+}
+
+
+/*
+Funcion para hacer la carga de los recursos primordiales en la primera etapa de inicio del sitio, cargu aqui lo primero que deba de ver el usuario y las funciones mas primordiales y becesarias del sitio.
+*/
+async function cargaPrimaria(){
+	try {
+		let [v1, v2] = await Promise.all([pantallasAni(), pantallasShow()]);
+		await Promise.all([pantalla1Ani(), toolsPimeraCarga()]);
+	} catch (error) {
+		console.error('Error ejecutando cargaPrimaria:', error);
 	}
 
+	return 'cargaPrimaria terminada.';
+}
+
+/*
+Funcion para ir iniciando o ejecutando todas las tareas del sitio mas globales brindando una capa de control centralizado.
+*/
+async function iniciador(){
+	try {
+		await cargaPrimaria();
+	} catch (error) {
+		console.error('Error ejecutando iniciador:', error);
+	}
+
+	try {
+		await cargaSecundaria();
+	} catch (error) {
+		console.error('Error ejecutando iniciador:', error);
+	}
 }
 
 
-
-
-
-
-
-
-
-
-//Colocacion de correo con cierto nivel de seguridad
-function openMailer(event) {
-	event.preventDefault();
-
-	var url = encodeURIComponent(destinatario = atob("aG9sYUB2ZW50b21hcXVpYS5jb20="));
-	window.location.href = "mailto:" + url;
-}
-
-
-
-
-
-
-let dbDudas = [];
-dbDudas.push({pregunta:"¿Necesito tener redes sociales para me ayuden a aumentar mis ventas?", respuesta:"Sí, ya que forman parte de nuestro sistema comprobado para que vendas más."});
-dbDudas.push({pregunta:"Qué redes sociales “ocupo”?", respuesta:"Por lo menos Facebook. Dependiendo del giro de tu negocio, también puedes necesitar Instagram. Tenemos comprobado que la combinación de ambas es una receta segura para lograr ventas."});
-dbDudas.push({pregunta:"¿Uds. pueden ayudarme a crear mis redes sociales en caso de no tenerlas?", respuesta:"Por supuesto. Te llevaremos paso a paso en el proceso y te daremos algunos consejos para que de aquí en adelante puedas crear todas las que desees."});
-dbDudas.push({pregunta:"¿Las quesadillas deben llevar queso?", respuesta:"Nuestro abogados recomiendan no entrar en esas polémicas, por lo que nuestra respuesta es “sigue tu corazón”."});
-dbDudas.push({pregunta:"Tengo redes, pero no tengo página web. ¿Es  necesaria?", respuesta:"No. Podemos incrementar tus ventas sólo con tus redes sociales. Pero considera que contar con un sitio web le da mucha más credibilidad a cualquier negocio."});
-dbDudas.push({pregunta:"Me es difícil crear los textos y contenido para mi página de internet. ¿Uds. me ayudan con eso?", respuesta:"Claro!! Te ayudamos con imágenes, íconos y los textos más seductores para tu público."});
-dbDudas.push({pregunta:"¿El único afortunado de contar con sus servicios es Jalisco?", respuesta:"Gracias a la tecnología podemos brindar servicios a cualquier parte de la República. Si gustas que vayamos a tomar algo, nuestro equipo se encuentra en CDMX y Guadalajara ;D"});
-dbDudas.push({pregunta:"¿Vienen, voy, nos vemos debajo del reloj, o cómo está la cosa?", respuesta:"Nosotros preferimos visitarte para conocer tu negocio y verlo en acción. Mientras más nos empapemos de lo que haces, tendremos más herramientas para desarrollar una mejor estrategia. "});
-
-
-
-
-let primerRetardo;
-let idaDomLoaded = false;
-let idaRequireLoaded = false;
-async function allLoaded() {
-	if(idaRequireLoaded && idaDomLoaded){
-		primerRetardo = setTimeout(() => {
-			const precarga = new Precarga();
-			precarga.userFunc = iniciar;
-			precarga.run();
-		}, 500)
-	};
-}
-
-
-
-
-function precargaActive(){
-	idaRequireLoaded = true;
-	allLoaded();
+/*
+Funcion donde se controla el sitio serb general con  otros eventos de carga o de moitoreo antes de dar inicio a la actiacion de funciones generales del sito.
+*/
+function controlador(){
+	// Registrar plugins de GSAP
+	gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, Flip);
+	
+	iniciador();
 };
-window.addEventListener('load', function() {
-	idaDomLoaded = true;
-	allLoaded();
-});
-
+const retrasoPregargaImg = 1000;
 
 
 
@@ -725,4 +754,4 @@ requirejs.config({
 		}
 	}
 });
-requirejs(["l/modernizr", "l/precarga", "l/brands.min", "l/solid.min", "l/fontawesome", "gsap", "ScrollTrigger", "ScrollToPlugin", "Flip"], precargaActive);
+requirejs(["l/modernizr", "l/precarga", "l/brands.min", "l/solid.min", "l/fontawesome", "gsap", "ScrollTrigger", "ScrollToPlugin", "Flip"], controlador);
